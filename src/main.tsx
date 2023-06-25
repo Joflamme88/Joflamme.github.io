4cfb9f83c724a59a47346411e81849da3549821f
Jonathan FLAMME <joflamme@hotmail.com>
2023-06-26 00:56:19 +0200

    CV Download

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createRoutesFromElements,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 
 import App from './components/App/App';
 
 import './styles/index.scss';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
